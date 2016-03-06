1ba8010955796577f7e5d9862806c781c6dfdf19
Dominic Reich <dominic@mm.st>
2016-03-06 12:59:28 +0100

    Veröffentlichung

--- Leitungslaengenberechnung/My Project/AssemblyInfo.vb
+++ Leitungslaengenberechnung/My Project/AssemblyInfo.vb
@@ -9,16 +9,16 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("Leitungslaengenberechnung")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("Leitungslaengenberechnung")> 
-<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Dominic Reich")>
+<Assembly: AssemblyProduct("Leitungslaengenberechnung")>
+<Assembly: AssemblyCopyright("Copyright © 2014 Dominic Reich")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("871e19c6-2dcd-494b-ba8b-2fb8a05db8ee")> 
+<Assembly: Guid("871e19c6-2dcd-494b-ba8b-2fb8a05db8ee")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.*")> 
+<Assembly: AssemblyVersion("1.0.*")>
 <Assembly: AssemblyFileVersion("1.0.0.0")> 
